f1b31600753eccf647daf240e2833c8f23eedec8
shashi-kant10 <shashiofficial10@gmail.com>
2021-01-29 16:28:01 +0530

    Added Custom Dialog

--- app/src/main/java/com/shashi/alertdialog/MainActivity.kt
+++ app/src/main/java/com/shashi/alertdialog/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.shashi.alertdialog
 
-import android.R
 import android.app.AlertDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
@@ -27,6 +28,11 @@ class MainActivity : AppCompatActivity() {
         //For full screen alert dialog
         binding.btnShowFullScreenAlert.setOnClickListener {
             showFullScreenAlertDialog()
+        }
+
+        //For custom alert dialog
+        binding.btnShowCustomAlert.setOnClickListener {
+            showCustomAlertDialog()
         }
 
     }
@@ -60,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
         val builder = AlertDialog.Builder(
             this,
-            R.style.Theme_Material_Light_NoActionBar_Fullscreen
+            android.R.style.Theme_Material_Light_NoActionBar_Fullscreen
         )
 
         builder
@@ -79,6 +85,35 @@ class MainActivity : AppCompatActivity() {
 
         val dialog = builder.create()
         dialog.show()
+
+    }
+
+    //For custom alert dialog
+    private fun showCustomAlertDialog() {
+
+        val builder = AlertDialog.Builder(this)
+
+        val view: View = layoutInflater.inflate(R.layout.custom_alert_layout, null)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+
+        val btnLater: Button = view.findViewById(R.id.btnLater)
+        btnLater.setOnClickListener {
+            showMessage("Job ke sath UPSC ka preparation karlo.")
+            dialog.dismiss()
+        }
+        val btnNo: Button = view.findViewById(R.id.btnNo)
+        btnNo.setOnClickListener {
+            showMessage("Very good. Waise shadi ka kya soche?")
+            dialog.dismiss()
+        }
+        val btnYes: Button = view.findViewById(R.id.btnYes)
+        btnYes.setOnClickListener {
+            showMessage("Galat dost bana liye ho lagta hai!")
+            dialog.dismiss()
+        }
 
     }
 
